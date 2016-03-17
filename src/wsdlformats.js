8acb464602474c370d1e1891730ea81c13988b08
Arturo Castillo <arturo@escaleno.cl>
2016-03-17 11:05:21 -0300

    wsdl schemas.

--- src/wsdlformats.js
+++ src/wsdlformats.js
@@ -1,37 +1,91 @@
 "use strict"
 
-export var initTransaction = {
-	wSTransactionType: '',//wsTransactionType
-    commerceId: '',//string
-    buyOrder: '',//string
-    sessionId: '',//string
-    returnURL: '',//anyURI
-    finalURL: '',//anyURI
-    transactionDetails: {
-    	sharesAmount: '',//decimal
-	    sharesNumber: '',//int
-	    amount: '',//decimal
-	    commerceCode: '',//string
-	    buyOrder: ''//string
-    },
-    wPMDetail: {
-    	serviceId: '',//string
-	    cardHolderId: '',//string
-	    cardHolderName: '',//string
-	    cardHolderLastName1: '',//string
-	    cardHolderLastName2: '',//string
-	    cardHolderMail: '',//string
-	    cellPhoneNumber: '',//string
-	    expirationDate: '',//dateTime
-	    commerceMail: '',//string
-	    ufFlag: ''//boolean
-    }
-}
-
 export var getTransactionResult = {
-	tokenInput: ''//string
+	tokenInput: ''
 }
 
 export var acknowledgeTransaction = {
-	tokenInput: ''//string	
+	tokenInput: ''
+}
+
+export var getTransactionResult = {
+	tokenInput: ''
+}
+
+export var getTransactionResultResponse = {
+	return: {}//transactionResultOutput
+}
+
+export var transactionResultOutput = {
+	accountingDate: '',
+	buyOrder: ''
+	cardDetail: {},
+	detailOutput: {},
+	sessionId: '',
+	transactionDate: '',//dateTime
+	urlRedirection: '',
+	VCI: ''
+}
+
+export var cardDetail = {
+	cardNumber: '',
+	cardExpirationDate: ''
+}
+
+export var wsTransactionDetailOutput = {
+	authorizationCode: '',
+	paymentTypeCode: '',
+	responseCode: 0
+}
+export var wsTransactionDetail = {
+	sharesAmount: 0.0,
+	sharesNumber: 0,
+	amount: 0.0,
+	commerceCode: '',
+	buyOrder: ''
+}
+
+export var acknowledgeTransaction = {
+	tokenInput: ''
+}
+
+export var acknowledgeTransactionResponse = {
+
+}
+
+export var initTransaction = {
+	wsInitTransactionInput: {}
+}
+
+export var wsInitTransactionInput = {
+	wSTransactionType: {},//wsTransactionType
+	commerceId: '',
+	buyOrder: '',
+	sessionId: '',
+	returnURL: '',
+	finalURL: '',
+	transactionDetails: {},//wsTransactionDetail
+	wPMDetail: {}//wpmDetailInput
+}
+
+export var wpmDetailInput = {
+	serviceId: '',
+	cardHolderId: '',
+	cardHolderName: '',
+	cardHolderLastName1: '',
+	cardHolderLastName2: '',
+	cardHolderMail: '',
+	cellPhoneNumber: '',
+	expirationDate: '',
+	commerceMail: '',
+	ufFlag: 0//boolean
+}
+
+export var initTransactionResponse = {
+	return: {}//wsInitTransactionOutput
+}
+
+export var wsInitTransactionOutput = {
+	token: '',
+	url: ''
 }
